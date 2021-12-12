336db582e6d58af4634ec084792980aa4e5e7e28
hannesfischer <hannesfischernec@gmail.com>
2021-12-12 12:06:42 +0100

    failed implementation of CSV File Reader

--- bomtool.py
+++ bomtool.py
@@ -1,10 +1,11 @@
-import sys
+import sys, csv, pandas as pd
 
 __ver = "1.0.0"
 
 arg_len = len(sys.argv)
+__arg_ok = 0
 
-if(arg_len != 3):
+if(arg_len != 5):
     _f = open("helptext.txt", "r")
     print("Python BoM-Tool V" + __ver)
     print(_f.read())
@@ -15,10 +16,20 @@ elif (arg_len == 2):
         _f = open("helptext.txt", "r")
         print("Python BoM-Tool V" + __ver)
         print(_f.read())
+        _f.close()
 else:  
-    bom_file = sys.argv[1]
-    database = sys.argv[2]
+    bom_file_pth = sys.argv[1]
+    database_pth = sys.argv[2]
+    csv_coloum = sys.argv[3]
+    xlsx_label = sys.argv[4]
+    __arg_ok = True
 
-    print(bom_file)
-    print(database)
-
+if __arg_ok == True:
+    #Read CSV File:
+    csv_file = open(bom_file_pth, encoding="UTF8")
+    bom_data = csv.reader(csv_file)
+    csv_file.close()
+    #xlsx_file = pd.read_excel(database_pth)
+    #data = pd.DataFrame(xlsx_file, columns=[xlsx_label])
+    #data_length = len(data)
+    print(bom_data)
